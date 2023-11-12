3fae16841e5ff3aa6469611ccf20332f17f93ab4
Missael Padilla <lmpadillar@gmail.com>
2023-11-11 20:16:05 -0800

    route name config

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/init', (req, res) => {
   res.send('¡Hola, mundo!');
 });
 
