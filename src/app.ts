4752d75f5f65849607b48145db05e109bdb4c4be
Missael Padilla <lmpadillar@gmail.com>
2023-11-11 21:09:38 -0800

    ruta

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/api/GetHello', (req, res) => {
   res.send('¡Hola, mundo!');
 });
 
