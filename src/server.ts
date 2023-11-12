bd8b2850d853a3b845230609c1558ceaa1cca075
Missael Padilla <lmpadillar@gmail.com>
2023-11-11 20:07:25 -0800

    second commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+// src/server.ts
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('¡Hola, mundo!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
